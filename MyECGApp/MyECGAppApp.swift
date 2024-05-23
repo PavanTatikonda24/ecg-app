41cb6d2a30db7dc3c43baf70a7be1a70302567a8
PavanTatikonda24 <pavan.tatikonda4@gmail.com>
2024-05-23 16:02:45 -0400

    Initial Commit

---
+++ MyECGApp/MyECGAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyECGAppApp.swift
+//  MyECGApp
+//
+//  Created by Pavan Tatikonda on 5/23/24.
+//
+
+import SwiftUI
+
+@main
+struct MyECGAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
